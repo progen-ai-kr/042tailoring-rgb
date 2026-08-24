8db6bc792ec9143062b8a703ff15ecd372ba0c91
박수훈 <86775077+PrakSuHun@users.noreply.github.com>
2026-08-25 08:44:52 +0900

    팀별 사이트 선택 기능 추가

--- brand-config.js
+++ brand-config.js
@@ -3,4 +3,8 @@ window.BRAND_SITE = Object.freeze({
   name: "042테일러링",
   adminUrl: "https://042tailoring-admin.progen-web.workers.dev/admin",
   siteUrl: "https://042tailoring-rgb.progen-web.workers.dev",
+  sites: [
+    { team: "aiai", url: "https://042tailoring-aiai.progen-web.workers.dev" },
+    { team: "RGB", url: "https://042tailoring-rgb.progen-web.workers.dev" },
+  ],
 });
